8ff220de6e96ca24e994e424c59cebe9800dbd18
林庆汉 <976983151@qq.com>
2017-09-21 19:19:40 +0800

    修改ListView的点击事件

--- Alarm/app/src/main/java/com/example/alarm/AlarmListAdapter.java
+++ Alarm/app/src/main/java/com/example/alarm/AlarmListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.alarm;
 
 import android.content.Context;
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -27,22 +28,23 @@ public class AlarmListAdapter extends ArrayAdapter<AlarmList> {
         super(context,textViewResourceId,objects);
         reId=textViewResourceId;
     }
-    public View getView(final int position, View convertView, ViewGroup parent){
+    public View getView(final int position, View convertView, final ViewGroup parent){
         final AlarmList Alarm=getItem(position);
         View view= LayoutInflater.from(getContext()).inflate(reId,parent,false);
         TextView event=view.findViewById(R.id.txt_list_event);
         TextView percent=view.findViewById(R.id.txt_list_percent);
-        Button button=view.findViewById(R.id.btn_list_Delete);
-        dbHelper=new MyDatabaseHelper(getContext(),"AlarmStore.db",null,1);
-        button.setOnClickListener(new View.OnClickListener() {
+     /*   Button button=view.findViewById(R.id.btn_list_Delete);
+       dbHelper=new MyDatabaseHelper(getContext(),"AlarmStore.db",null,1);
+          button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
          //       Toast.makeText(getContext(),"mmgdh"+String.valueOf(Alarm.getId()),Toast.LENGTH_SHORT).show();
                 SQLiteDatabase db=dbHelper.getWritableDatabase();
                 db.delete("Alarm","id=?",new String[] {String.valueOf(Alarm.getId())});
-
+                Intent intent=new Intent(getContext(),MainActivity.class);
+                getContext().startActivity(intent);
             }
-        });
+        });*/
         event.setText(Alarm.getEvent());
         percent.setText(String.valueOf(Alarm.getTime()));
         return view;
--- Alarm/app/src/main/java/com/example/alarm/MainActivity.java
+++ Alarm/app/src/main/java/com/example/alarm/MainActivity.java
@@ -6,10 +6,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabaseLockedException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private MyDatabaseHelper dbHelper;
+    private List<AlarmList> alarmList=new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,6 @@ public class MainActivity extends AppCompatActivity {
 
         dbHelper=new MyDatabaseHelper(this,"AlarmStore.db",null,1);
 
-        Select_Data();
         //点击事件--创建数据库
         Button createDatabase=(Button)findViewById(R.id.create_database);
         createDatabase.setOnClickListener(new View.OnClickListener() {
@@ -42,31 +44,41 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent=new Intent();
                 intent.setClass(MainActivity.this,View_Add.class);
                 startActivity(intent);
+                finish();
             }
         });
-        //查询数据
-
-
-
-    }
-    public void Select_Data(){
-        List<AlarmList> alarmList=new ArrayList<>();
+        //显示数据
         alarmList.clear();
         SQLiteDatabase db=dbHelper.getWritableDatabase();
         Cursor cursor=db.query("Alarm",null,null,null,null,null,null);
         AlarmListAdapter adapter=new AlarmListAdapter(MainActivity.this,R.layout.activity_list_view,alarmList);
         ListView listView=(ListView)findViewById(R.id.list_view);
-
-
         listView.setAdapter(adapter);
         if (cursor.moveToFirst()){
             do {
-               String event=cursor.getString(cursor.getColumnIndex("event"));
-               int time=cursor.getInt(cursor.getColumnIndex("total_time"));
+                String event=cursor.getString(cursor.getColumnIndex("event"));
+                int time=cursor.getInt(cursor.getColumnIndex("total_time"));
                 int id=cursor.getInt(cursor.getColumnIndex("id"));
                 AlarmList list=new AlarmList(id,event,time);
                 alarmList.add(list);
             }while (cursor.moveToNext());
         }
+        //listView点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                AlarmList alarm=alarmList.get(i);
+                Log.d("aa",String.valueOf(alarm.getId()));
+                Toast.makeText(MainActivity.this,String.valueOf(alarm.getId()),Toast.LENGTH_SHORT).show();
+            }
+        });
+        //listView里button的点击事件--删除
+        Button button=(Button)findViewById(R.id.btn_list_Delete);
+
+
+
+
     }
+
 }
